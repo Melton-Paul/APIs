f3ec47ced216dcb9839739106f173793a12bcac6
Paul Melton <melton.paul@hotmail.com>
2022-04-20 15:40:11 -0500

    Set cards to pull from local storage.

--- js/war.js
+++ js/war.js
@@ -1,10 +1,22 @@
-const getCards = document.getElementById("getCards")
+let deckId = window.localStorage.getItem('ID')
+document.getElementById("getCards").addEventListener("click", fetchDeck)
+document.getElementById("drawCards").addEventListener("click", drawCards)
 
-
-getCards.addEventListener("click", fetchCards)
-
-function fetchCards() {
+function fetchDeck() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+    .then(res => res.json())
+    .then(data => {
+        window.localStorage.clear()
+        window.localStorage.setItem('ID', data.deck_id)
+        deckId = window.localStorage.getItem('ID')
+        })
+}
+
+function drawCards() {
+    if(deckId){
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => console.log(data))} else {
+            console.log("You have to draw deck first!")
+        }
 }
